7c717a639239b469c87c819362a864a8a110bcdb
Naved Ali <qa.navedali@gmail.com>
2018-07-11 19:05:34 +0530

    Create InCaseSensitiveXPath.java

---
+++ AllProjectInOne/src/com/AllProjectInOne/scripts/InCaseSensitiveXPath.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InCaseSensitiveXPath
+{
+
+	WebDriver driver=null;
+
+	@Test
+	public void inCaseSensitiveXPath() throws Exception
+	{
+
+		WebDriverManager.chromedriver().setup();
+
+		ChromeOptions options = new ChromeOptions();
+
+		options.addArguments("--disable-infobars");
+
+		driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+
+		driver.navigate().to("https://www.google.com");
+
+		Thread.sleep(5000);
+
+		System.out.println("\n\nNormal xpath : "+new SimpleDateFormat("dd-MM-yyy HH:mm:ss:SSSSSSSSSS").format(Calendar.getInstance().getTime())+"\n\n");
+
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("demo ");
+
+		System.out.println("\n\nNormal xpath end : "+new SimpleDateFormat("dd-MM-yyy HH:mm:ss:SSSSSSSSSS").format(Calendar.getInstance().getTime())+"\n\n");
+
+		driver.findElement(By.name("q")).sendKeys("demo ");
+		
+		System.out.println("\n\nID xpath end : "+new SimpleDateFormat("dd-MM-yyy HH:mm:ss:SSSSSSSSSS").format(Calendar.getInstance().getTime())+"\n\n");
+		
+		driver.findElement(By.xpath("//input[translate(@name, 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')='Q']")).sendKeys("demo ");
+		
+		System.out.println("\n\ntranslate xpath end : "+new SimpleDateFormat("dd-MM-yyy HH:mm:ss:SSSSSSSSSS").format(Calendar.getInstance().getTime())+"\n\n");
+
+	}
+
+	@AfterMethod
+	public void quitDriver()
+	{
+		driver.quit();
+	}
+}
